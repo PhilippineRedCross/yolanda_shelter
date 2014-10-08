48ca37c742ae4b0a4abfa33a8b8c90166172f04f
Dan Joseph <danbjoseph@gmail.com>
2014-10-08 13:00:20 +0800

    demographic tables

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var pageHeight = $(window).height();
-$("#map").css("height", pageHeight * 0.80 );
+$("#map").css("height", pageHeight * 0.70 );
 
 var mapUrl = 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
 var mapAttribution = 'Map data &copy; <a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a> | Map style by <a href="http://hot.openstreetmap.org" target="_blank">H.O.T.</a> | &copy; <a href="https://ifrc.org/" title="IFRC" target="_blank">IFRC</a> 2014 | <a title="Disclaimer" onClick="showDisclaimer();">Disclaimer</a>';
@@ -32,12 +32,16 @@ map.addControl(loadingControl);
 // ================
 var allPartners = true;
 var allProvinces = true;
+var allMunicip = true;
+var allBarangays = true;
 var shelters = [];
+var displayedShelters = [];
+var demographicData = [];
 var markersBounds = [];
 var markers = new L.MarkerClusterGroup().addTo(map);
 var centroidOptions = {
     radius: 8,
-    fillColor: "#ED1B2E",
+    fillColor: "#a50f15",
     color: "#FFF",
     weight: 2.5,
     opacity: 1,
@@ -46,6 +50,9 @@ var centroidOptions = {
 
 // helper functions
 // ================
+
+// comma seperator for thousands
+var formatCommas = d3.format(",");
 
 // on marker click open modal
 function markerClick (e) {
@@ -84,8 +91,15 @@ function showDisclaimer() {
 // function chain for map
 // ======================
 
-// get CSV file
-function getData() {
+// get CSV files
+function getData(){
+  d3.csv("data/demographic-data.csv", function(data){ 
+    demographicData = data;
+    getMapData(); 
+  });
+}
+
+function getMapData() {
 	d3.csv("data/shelter-data.csv", function(data){ 
 		formatData(data); 
 	});
@@ -121,91 +135,101 @@ function formatData(data){
 
 //build partners buttons
 function parsePartners() {
-  var partnerCounts = {};
   var partnerList = [];
   $(shelters).each(function(index, record){
-    var partnerName = record.properties.partner;
-    if (partnerName in partnerCounts){
-      partnerCounts[partnerName] += 1;
-    } else {
-      partnerCounts[partnerName] = 1;
-      partnerList.push(partnerName);      
-    } 
+    if ($.inArray(record.properties.partner, partnerList) === -1){
+      partnerList.push(record.properties.partner); 
+    }           
   });
   var partnerFilterHtml = "";
   partnerList.sort();
   $.each(partnerList, function(index, partner){
-    var itemHtml = '<button id="'+partner+'" class="btn btn-sm btn-donor" type="button" onclick="toggleFilter('+"'partners'"+', this);">'+
+    var itemHtml = '<button id="'+partner+'" class="btn btn-xs btn-donor" type="button" onclick="toggleFilter('+"'partners'"+', this);">'+
         partner + 
         ' <span class="glyphicon glyphicon-unchecked" style="margin-left:4px;"></span></button>';
     partnerFilterHtml += itemHtml;    
   });
   $('#partnerButtons').html(partnerFilterHtml);
-
   parseProvinces();
 }
-
 
 function parseProvinces() {
   var provinceInfo = {};
   $(shelters).each(function(index, record){
     var admin2 = record.properties.admin2;
-    // var provinceName = record.properties.province
     if (admin2 in provinceInfo){
-      provinceInfo[admin2].count += 1;
+      return;
     } else {
-      provinceInfo[admin2] = {};
-      provinceInfo[admin2].count = 1;
-      provinceInfo[admin2].name = record.properties.province;      
+      provinceInfo[admin2] = record.properties.province;      
     } 
   });
   var provinceFilterHtml = "";
   $.each(provinceInfo, function(index, province){
-    var itemHtml = '<button id="'+index+'" class="btn btn-sm btn-donor" type="button" onclick="toggleFilter('+"'provinces'"+', this);">'+
-        province.name + 
+    var itemHtml = '<button id="'+index+'" class="btn btn-xs btn-province" type="button" onclick="toggleFilter('+"'provinces'"+', this);">'+
+        province + 
         ' <span class="glyphicon glyphicon-unchecked" style="margin-left:4px;"></span></button>';
     provinceFilterHtml += itemHtml;    
   });
   $('#provinceButtons').html(provinceFilterHtml);
-
   markersToMap();
 }
 
-// function parseMunicip(pcode) {
-//   $('#municipButtonsWrapper').empty();
-//   var municipInfo = {};
-//   var admin2 = pcode;
-//   $(shelters).each(function(index, record){
-//     var thisAdmin2 = record.properties.admin2;
-//     var admin3 = record.properties.admin3;
-//     if (admin2 === thisAdmin2){
-//       if (admin3 in municipInfo){
-//         municipInfo[admin3].count += 1;
-//       } else {
-//         municipInfo[admin3] = {};
-//         municipInfo[admin3].count = 1;
-//         municipInfo[admin3].name = record.properties.municipality;      
-//       } 
-//     }
-//   var municipFilterHtml = '<h6><b>Municipalities:</b><span  id="municipButtons" style="padding-left:10px;">' ;
-//   $.each(municipInfo, function(index, municip){
-//     var itemHtml = '<button id="'+index+'" class="btn btn-sm btn-municip" type="button" onclick="toggleFilter('+"'municipalities'"+', this);">'+
-//         municip.name +' (' + municip.count + ')'+ 
-//         '<span class="glyphicon glyphicon-unchecked" style="margin-left:4px;"></span></button>';
-//     municipFilterHtml += itemHtml;    
-//   });
-//   municipFilterHtml += '</span></h6>';
-//   $('#municipButtonsWrapper').html(municipFilterHtml);
-    
-//   });
-// }
+function parseMunicip(pcode) {
+  $('#municipButtonsWrapper').empty();
+  var municipInfo = {};
+  var admin2 = pcode;
+  $(shelters).each(function(index, record){
+    var thisAdmin2 = record.properties.admin2;
+    var admin3 = record.properties.admin3;
+    if (admin2 === thisAdmin2){
+      if (admin3 in municipInfo){
+        return;
+      } else {
+        municipInfo[admin3] = record.properties.municipality;      
+      } 
+    }
+  });
+  var municipFilterHtml = '<h6><b>Municipalities:</b><span  id="municipButtons" style="padding-left:10px;">' ;
+  $.each(municipInfo, function(index, municip){
+    var itemHtml = '<button id="'+index+'" class="btn btn-xs btn-municip" type="button" onclick="toggleFilter('+"'municipalities'"+', this);">'+
+        municip +
+        '<span class="glyphicon glyphicon-unchecked" style="margin-left:4px;"></span></button>';
+    municipFilterHtml += itemHtml;    
+  });
+  municipFilterHtml += '</span></h6>';
+  $('#municipButtonsWrapper').html(municipFilterHtml);   
+}
           
+function parseBarangays(pcode) {
+  $('#barangayButtonsWrapper').empty();
+  var brgyInfo = {};
+  var admin3 = pcode;
+  $(shelters).each(function(index, record){
+    var thisAdmin3 = record.properties.admin3;
+    var admin4 = record.properties.admin4;
+    if (admin3 === thisAdmin3){
+      if (admin4 in brgyInfo){
+        return;
+      } else {
+        brgyInfo[admin4] = record.properties.barangay;      
+      } 
+    }
+  });
+  var brgyFilterHtml = '<h6><b>Barangays:</b><span  id="barangayButtons" style="padding-left:10px;">' ;
+  $.each(brgyInfo, function(index, brgy){
+    var itemHtml = '<button id="'+index+'" class="btn btn-xs btn-brgy" type="button" onclick="toggleFilter('+"'barangays'"+', this);">'+
+        brgy +
+        '<span class="glyphicon glyphicon-unchecked" style="margin-left:4px;"></span></button>';
+    brgyFilterHtml += itemHtml;    
+  });
+  brgyFilterHtml += '</span></h6>';
+  $('#barangayButtonsWrapper').html(brgyFilterHtml);  
+}
 
-function toggleFilter(filter, element) {
-  
+
+function toggleFilter(filter, element) {  
   var filterbuttons;
   var filterAllFilter;
-
   switch(filter){
     case 'partners':
       filterButtons = $("#partnerButtons").children(); 
@@ -214,6 +238,16 @@ function toggleFilter(filter, element) {
     case 'provinces':
       filterButtons = $("#provinceButtons").children();
       allProvinces = false;
+      parseMunicip($(element).attr("id"));
+      break;
+    case 'municipalities':
+      filterButtons = $("#municipButtons").children();
+      parseBarangays($(element).attr("id"));
+      allMunicip = false;
+      break;
+    case 'barangays':
+      filterButtons = $("#barangayButtons").children();
+      allBarangays = false;
       break;
   } 
   if($(element).hasClass("filtering") !== true){
@@ -239,12 +273,23 @@ function toggleFilter(filter, element) {
         break; 
       case 'provinces':
         allProvinces = true;
+        allMunicip = true;
+        allBarangays = true;
+        $('#municipButtonsWrapper').empty();
+        $('#barangayButtonsWrapper').empty();
+        break;
+      case 'municipalities':
+        allMunicip = true;
+        allBarangays = true;
+        $('#barangayButtonsWrapper').empty();
+        break;
+      case 'barangays':
+        allBarangays = true;
         break;
     }    
   }
   markersToMap();
 }
-
 
 function resetFilters(){
   allPartners = true;
@@ -259,9 +304,78 @@ function resetFilters(){
     $(button).children().addClass("glyphicon-unchecked");
     $(button).removeClass("filtering");
   });
+  $('#municipButtonsWrapper').empty();
+  $('#barangayButtonsWrapper').empty();
   markersToMap();
 }
 
+function generateGraphs(){
+  var admin4List = [];
+  $.each(displayedShelters, function(index, shelter){
+    if($.inArray(shelter.properties.admin4, admin4List) === -1){
+      admin4List.push(shelter.properties.admin4);
+    }
+  });
+  var maleYoung = 0;
+  var maleChild = 0;
+  var maleAdult = 0;
+  var maleSenior = 0;
+  var femaleYoung = 0;
+  var femaleChild = 0;
+  var femaleAdult = 0;
+  var femaleSenior = 0;
+  var pregnant = 0;
+  var mSingleHead = 0;
+  var fSingleHead = 0;
+  var mDisabled = 0;
+  var fDisabled = 0;
+  $.each(demographicData, function(index, household){
+    if($.inArray(household.admin4, admin4List) !== -1){
+      maleYoung += parseInt(household.male_young, 10);
+      maleChild += parseInt(household.male_child, 10);
+      maleAdult += parseInt(household.male_adult, 10);
+      maleSenior += parseInt(household.male_senior, 10);
+      femaleYoung += parseInt(household.female_young, 10);
+      femaleChild += parseInt(household.female_child, 10);
+      femaleAdult += parseInt(household.female_adult, 10);
+      femaleSenior += parseInt(household.female_senior, 10); 
+      pregnant += parseInt(household["vulnerabilities/pregnant_lactating"], 10);     
+      mDisabled += parseInt(household["vulnerabilities/disabled_ill_male"], 10);
+      fDisabled += parseInt(household["vulnerabilities/disabled_ill_female"], 10);
+      if(household["vulnerabilities/single_headed"] === "yes"){
+        switch(household["vulnerabilities/single_headed_sex"]){
+          case 'male':
+            mSingleHead += 1;
+            break;
+          case 'female':
+            fSingleHead += 1;
+            break;
+        }
+      }
+    }
+  });
+  var femaleTotal = femaleYoung + femaleChild + femaleAdult + femaleSenior;
+  var maleTotal = maleYoung + maleChild + maleAdult + maleSenior;
+  var grandTotal = femaleTotal + maleTotal;
+  $("#maleYoung").html(formatCommas(maleYoung));
+  $("#maleChild").html(formatCommas(maleChild));
+  $("#maleAdult").html(formatCommas(maleAdult));
+  $("#maleSenior").html(formatCommas(maleSenior));
+  $("#femaleYoung").html(formatCommas(femaleYoung));
+  $("#femaleChild").html(formatCommas(femaleChild));
+  $("#femaleAdult").html(formatCommas(femaleAdult));
+  $("#femaleSenior").html(formatCommas(femaleSenior));
+  $("#pregnant").html(formatCommas(pregnant));
+  $("#mSingleHead").html(formatCommas(mSingleHead));
+  $("#fSingleHead").html(formatCommas(fSingleHead));
+  $("#mDisabled").html(formatCommas(mDisabled));
+  $("#fDisabled").html(formatCommas(fDisabled));
+  $("#mSingleHead").html(formatCommas(mSingleHead));
+  $("#fSingleHead").html(formatCommas(fSingleHead));
+  $("#maleTotal").html(formatCommas(maleTotal));
+  $("#femaleTotal").html(formatCommas(femaleTotal));
+  $("#grandTotal").html(formatCommas(grandTotal));
+}
 
 function markersToMap(){
     map.removeLayer(markers);
@@ -270,22 +384,27 @@ function markersToMap(){
         maxClusterRadius: 20,   
         spiderfyDistanceMultiplier:2
     });    
-    var displayedShelters = [];
+    displayedShelters = [];
     var partnerFilter = $("#partnerButtons").find(".filtering").attr("id");
     var provinceFilter = $("#provinceButtons").find(".filtering").attr("id");
+    var municipFilter = $("#municipButtons").find(".filtering").attr("id");
+    var barangayFilter = $("#barangayButtons").find(".filtering").attr("id");
     $.each(shelters, function (index, shelter){
       if(shelter.properties.partner === partnerFilter || allPartners === true){
         if(shelter.properties.admin2 === provinceFilter || allProvinces === true){
-          displayedShelters.push(shelter);
+          if(shelter.properties.admin3 === municipFilter || allMunicip === true){
+            if(shelter.properties.admin4 === barangayFilter || allBarangays === true){
+              displayedShelters.push(shelter);
+            }
+          }  
         }
       }
     });
     // update count
     var displayedCount = displayedShelters.length.toString();
     $("#mappedCount").html(displayedCount);
-
     //disable buttons
-    var possibleFilters = ["ALL-PARTNERS", "ALL-PROVINCES"];
+    var possibleFilters = [];
     var filterProperties = ["partner", "admin2", "admin3", "admin4"];
     $.each(displayedShelters, function(index, shelter){
       $.each(filterProperties, function(index2, property){
@@ -294,7 +413,7 @@ function markersToMap(){
         } 
       }); 
     });    
-    var filterGroups = ["#partnerButtons", "#provinceButtons"];
+    var filterGroups = ["#partnerButtons", "#provinceButtons", "#municipButtons", "#barangayButtons"];
     $.each(filterGroups, function(index, grouping){
       var thisButtons = $(grouping).children();
       thisButtons.removeClass("disabled");
@@ -319,11 +438,8 @@ function markersToMap(){
     markers.addLayer(marker);
     markers.addTo(map);
     markersBounds = markers.getBounds();
-    markersBounds._northEast.lat += 0.08;
-    markersBounds._northEast.lng += 0.08;
-    markersBounds._southWest.lat -= 0.08;
-    markersBounds._southWest.lat -= 0.08;
-    map.fitBounds(markersBounds);    
+    map.fitBounds(markersBounds); 
+    generateGraphs();   
 } 
 
 //start function chain for map
